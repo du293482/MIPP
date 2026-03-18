9d40982c349d4bc48112140162b59f03bc3822f4
06m29 <du293482@gmail.com>
2026-03-18 17:34:49 +0900

    퍼블리싱 업데이트

--- src/main/webapp/js/mi-view.js
+++ src/main/webapp/js/mi-view.js
@@ -1,20 +1,46 @@
 const ctx = document.getElementById('mi-view-canvas');
 
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
-    },
+const labels = [10,20,30,40,50,60,70];
+
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: [1,2,3,4,5,6,7],
+            borderColor: '#00ff00',
+            backgroundColor: '#00aa00',
+        },
+        {
+            label: 'Dataset 2',
+            data: [7,6,5,4,3,2,1],
+            borderColor: '#ff0000',
+            backgroundColor: '#aa0000',
+        }
+    ]
+};
+
+const config = {
+    type: 'line',
+    data: data,
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        responsive: true,
+
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            align: 'inner',
+            title: {
+                display: true,
+                text: 'Unit: $/MMBtu'
             }
         }
-    }
+    },
+};
+
+new Chart(ctx, {
+    type: config.type,
+    data: data,
+    options: config.options
 });
